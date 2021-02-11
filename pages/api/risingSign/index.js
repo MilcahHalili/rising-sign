30cf04574c34008783be4bb75f361f8ada50b5c7
Milcah <halilicenteno@gmail.com>
2021-02-11 14:56:35 -0800

    remove boilerplate api route

--- pages/api/risingSign/index.js
+++ pages/api/risingSign/index.js
@@ -1,4 +1,5 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
 export default function handler(req, res) {
-  console.log(req.body)
-  res.status(200).json(req.body)
+  res.status(200).json({ status: 'risingSign api route success'})
 }
