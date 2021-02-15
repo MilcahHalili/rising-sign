cef73a9eecbbdd03183e7f0b35ad9dbdfd384f76
Milcah <halilicenteno@gmail.com>
2021-02-15 15:04:25 -0800

    change api env var

--- pages/api/ascendant/index.js
+++ pages/api/ascendant/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 export default async function handler(req, res, err) {
-  const url = process.env.API_URL
+  const url = process.env.CUSTOM_API_URL
 
   if (req.method === 'POST') {
     const response = await axios.post(url, {
